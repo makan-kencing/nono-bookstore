11c68d9b6f6d04cb9b32074859ac820be582c937
makan-kencing <dizzynight2096@gmail.com>
2025-06-23 11:40:47 +0800

    initial commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'vendor/autoload.php';
+
+Flight::route('/', function() {
+    echo 'hello world!';
+});
+
+Flight::route('/json', function() {
+    Flight::json(['hello' => 'world']);
+});
+
+Flight::start();
