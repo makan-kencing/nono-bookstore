04ad9dbe45e2a570175fed53fe032598e57162b0
makan-kencing <dizzynight2096@gmail.com>
2025-06-26 14:32:59 +0800

    feat: simple routing with fastroute

--- web/config/routes.php
+++ web/config/routes.php
@@ -1,3 +1,13 @@
 <?php declare(strict_types=1);
 
-
+define("ROUTES", [
+    ['GET', '/', function () {
+        echo 'Index';
+    }],
+    ['GET', '/hello-world', function () {
+        echo 'Hello World';
+    }],
+    ['GET', '/another-route', function () {
+        echo 'This works too';
+    }],
+]);
--- web/public/index.php
+++ web/public/index.php
@@ -14,4 +14,36 @@ else
     });
 $whoops->register();
 
+$routeDefinitionCallback = function (\FastRoute\RouteCollector $r) {
+    foreach (ROUTES as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+};
+
+$dispatcher = FastRoute\simpleDispatcher($routeDefinitionCallback);
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
 
