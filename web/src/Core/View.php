b6adebed4a1096ad06925ad1a10a48258f0a3bf3
makan-kencing <dizzynight2096@gmail.com>
2025-06-26 22:16:49 +0800

    feat: use simple view renderer

---
+++ web/src/Core/View.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Core;
+
+class View
+{
+    public static function render(string $file, array $data = []) : string
+    {
+        $path = '../views/' . $file;
+
+        ob_start();
+        extract($data);
+        include $path;
+        return ob_get_clean();
+    }
+}
