2cd6921bb7945e2b2f1f732536cfb125f3d3f92a
makan-kencing <dizzynight2096@gmail.com>
2025-06-26 14:23:17 +0800

    feat: add whoops for dev error

--- web/public/index.php
+++ web/public/index.php
@@ -3,3 +3,15 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/routes.php';
 require_once __DIR__ . '/../config/config.php';
+
+
+$whoops = new Whoops\Run;
+if (DEBUG)
+    $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler);
+else
+    $whoops->pushHandler(function ($e) {
+        echo 'Todo: Friendly error page and send an email to the developer';
+    });
+$whoops->register();
+
+
