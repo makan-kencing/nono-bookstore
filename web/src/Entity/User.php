f52bcb3986066fb6d76e3bb1d93e1a8bc7439407
makan-kencing <dizzynight2096@gmail.com>
2025-06-30 19:12:56 +0800

    style: remove whitespace

--- web/src/Entity/User.php
+++ web/src/Entity/User.php
@@ -6,17 +6,12 @@ use DateTime;
 
 class User
 {
-    private  int $id;
+    private int $id;
     private string $username;
     private string $email;
-
     private string $hashed_password;
-
     private Role $role;
-
     private bool $is_verified = false;
-
     private string $last_ip;
-
     private DateTime $last_login;
 }
