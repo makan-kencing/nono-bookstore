94cefb5725d1220a61adde824fe21e7e8454fe3c
makan-kencing <dizzynight2096@gmail.com>
2025-07-14 18:32:45 +0800

    refactor: style to phpstan

--- web/public/index.php
+++ web/public/index.php
@@ -1,39 +1,46 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/routes.php';
 require_once __DIR__ . '/../config/config.php';
 
+use App\Core\View;
+use FastRoute\RouteCollector;
 
-$whoops = new Whoops\Run;
-if (DEBUG)
-    $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler);
-else
+$whoops = new Whoops\Run();
+if (DEBUG) {
+    $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+} else {
     $whoops->pushHandler(function ($e) {
         http_response_code(500);
-        echo \App\Core\View::render("errors/500.php");
+        echo View::render("errors/500.php");
     });
+}
 $whoops->register();
 
 
-$dispatcher = FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
-    foreach (ROUTES as $route)
+$dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
+    foreach (ROUTES as $route) {
         $r->addRoute($route[0], $route[1], $route[2]);
+    }
 });
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
 // Strip query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?'))
+if (false !== $pos = strpos($uri, '?')) {
     $uri = substr($uri, 0, $pos);
+}
 $uri = rawurldecode($uri);
 
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         http_response_code(404);
-        echo \App\Core\View::render("errors/404.php");
+        echo View::render("errors/404.php");
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
@@ -45,8 +52,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className;
+        $class = new $className();
         $class->$method($vars);
         break;
 }
-
