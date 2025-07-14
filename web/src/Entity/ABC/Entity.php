35a49f8e8a4cc0472a7e2faa7fcdd77cd847f0ef
makan-kencing <dizzynight2096@gmail.com>
2025-07-14 13:38:40 +0800

    feat: add lazy load marker?

--- web/src/Entity/ABC/Entity.php
+++ web/src/Entity/ABC/Entity.php
@@ -7,9 +7,11 @@ namespace App\Entity\ABC;
 abstract class Entity
 {
     public readonly ?int $id;
+    public readonly ?bool $isLazy;
 
     public function __construct(?int $id = null)
     {
+        $this->isLazy = true;
         $this->id = $id;
     }
 }
