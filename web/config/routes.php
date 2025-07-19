cef6a852a619aad41dd6724c7f7e844b1d05b1a4
makan-kencing <dizzynight2096@gmail.com>
2025-07-19 14:38:05 +0800

    refactor: type annotate ROUTES

--- web/config/routes.php
+++ web/config/routes.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+/**
+ * @var list<array{0: string|string[], 1: string, 2: array{0: class-string, 1: callable-string}}
+ */
 const ROUTES = [
-    ['GET', '/', ['App\Controller\HelloController', 'index']]
+    ['GET', '/', ['App\Controller\HomeController', 'index']]
 ];
