8266528513b4eb66d48036a705305d6b9c97b77c
makan-kencing <dizzynight2096@gmail.com>
2025-07-19 14:38:51 +0800

    feat: add PDO connection

--- web/public/index.php
+++ web/public/index.php
@@ -8,15 +8,26 @@ require_once __DIR__ . '/../config/routes.php';
 use App\Core\View;
 use FastRoute\RouteCollector;
 
-View::$viewPath = __DIR__ . "/../views";
+$view = new View();
+$view::$viewPath = __DIR__ . "/../views";
+
+$pdo = new PDO(
+    'mysql:dbname=' . $_ENV['DATABASE_NAME'] . ';host=' . $_ENV['DATABASE_HOST'] . ';port=' . $_ENV['DATABASE_PORT'],
+    $_ENV['DATABASE_USER'],
+    $_ENV['DATABASE_PASSWORD'],
+    [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    ]
+);
 
 $whoops = new Whoops\Run();
 if ($_ENV['APP_ENV'] == 'dev') {
     $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
 } else {
     $whoops->pushHandler(function ($e) {
+        global $view;
         http_response_code(500);
-        echo View::render("errors/500.php");
+        echo $view->render("errors/500.php");
     });
 }
 $whoops->register();
@@ -41,7 +52,7 @@ $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
         http_response_code(404);
-        echo View::render("errors/404.php");
+        echo $view->render("errors/404.php");
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
         $allowedMethods = $routeInfo[1];
@@ -49,11 +60,20 @@ switch ($routeInfo[0]) {
         http_response_code(405);
         break;
     case FastRoute\Dispatcher::FOUND:
+        /**
+         * @var class-string<Controller> $className
+         */
         $className = $routeInfo[1][0];
+        /**
+         * @var callable-string<Controller> $method
+         */
         $method = $routeInfo[1][1];
+        /**
+         * @var array<string, mixed> $vars
+         */
         $vars = $routeInfo[2];
 
-        $class = new $className();
+        $class = new $className($pdo, $view);
         $class->$method($vars);
         break;
 }
