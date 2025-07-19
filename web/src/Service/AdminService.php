647e427ec68ae2d67d2ff110c0ea1092754ef38d
makan-kencing <dizzynight2096@gmail.com>
2025-07-19 15:13:32 +0800

    refactor: add declare(strict_types=1)

--- web/src/Service/AdminService.php
+++ web/src/Service/AdminService.php
@@ -1,13 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service;
 
 class AdminService
 {
-    public function viewUserList($vars) :void{}
-    public function addStaff($vars) :void{}
-    public function delStaff($vars) :void{}
-    public function editUser($vars) :void{}
-    public function addMember($vars) :void{}
-    public function delMember($vars) :void{}
+    public function viewUserList($vars): void
+    {
+    }
+
+    public function addStaff($vars): void
+    {
+    }
+
+    public function delStaff($vars): void
+    {
+    }
+
+    public function editUser($vars): void
+    {
+    }
+
+    public function addMember($vars): void
+    {
+    }
+    public function delMember($vars): void
+    {
+    }
 }
