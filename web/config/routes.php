eaaf1df22f6a6550a6a7dfa5fd8665148d6caabb
makan-kencing <dizzynight2096@gmail.com>
2025-07-27 00:27:41 +0800

    feat: added and updated some endpoints

--- web/config/routes.php
+++ web/config/routes.php
@@ -6,8 +6,8 @@ declare(strict_types=1);
  * @var list<array{0: string|string[], 1: string, 2: array{0: class-string, 1: callable-string}}>
  */
 const ROUTES = [
+    // public webpages
     ['GET', '/', ['App\Controller\HomeController', 'index']],
-
     ['GET', '/book/{isbn:\d{13}}[/{slug:[a-z0-9-]+}]', ['App\Controller\BookController', 'viewBook']],
     ['GET', '/search', ['App\Controller\BookController', 'searchBook']],
     ['GET', '/orders', ['App\Controller\OrderController', 'viewOrders']],
@@ -19,12 +19,22 @@ const ROUTES = [
     ['GET', '/author/{slug:[a-z0-9-]+}', ['App\Controller\AuthorController', 'viewAuthor']],
     ['GET', '/series/{slug:[a-z0-9-]+}', ['App\Controller\SeriesController', 'viewSeries']],
 
+    // admin webpages
     ['GET', '/admin/users', ['App\Controller\AdminUserController', 'viewUserList']],
 
+    // public api endpoints
     ['GET', '/api/user/exists', ['App\Controller\AdminUserController', 'checkUsernameExists']],
-    ['POST', '/api/cart/add', ['App\Controller\CartController', 'addItem']],
-    ['POST', '/api/cart/subtract', ['App\Controller\CartController', 'subtractItem']],
-    ['POST', '/api/cart/remove', ['App\Controller\CartController', 'removeItem']],
-    ['POST', '/api/wishlist/add', ['App\Controller\WishlistController', 'addItem']],
-    ['POST', '/api/wishlist/remove', ['App\Controller\WishlistController', 'removeItem']]
+    ['POST', '/api/cart/item', ['App\Controller\CartController', 'addItem']],
+    ['PUT', '/api/cart/item', ['App\Controller\CartController', 'subtractItem']],
+    ['DELETE', '/api/cart/item', ['App\Controller\CartController', 'removeItem']],
+    ['POST', '/api/wishlist/item', ['App\Controller\WishlistController', 'addItem']],
+    ['DELETE', '/api/wishlist/item', ['App\Controller\WishlistController', 'removeItem']],
+    ['POST', '/api/rating', ['App\Controller\RatingController', 'submitRating']],
+    ['PUT', '/api/rating', ['App\Controller\RatingController', 'editRating']],
+    ['DELETE', '/api/rating', ['App\Controller\RatingController', 'deleteRating']],
+    ['POST', '/api/reply', ['App\Controller\ReplyController', 'submitReply']],
+    ['PUT', '/api/reply', ['App\Controller\ReplyController', 'editReply']],
+    ['DELETE', '/api/reply', ['App\Controller\ReplyController', 'deleteReply']]
+
+    // admin api endpoints
 ];
