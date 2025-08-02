7f27c0338e5d913d833717d6e952a47d91ff2560
makan-kencing <dizzynight2096@gmail.com>
2025-08-02 13:30:03 +0800

    chore: remove routes.php

--- web/public/index.php
+++ web/public/index.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../config/routes.php';
 require_once __DIR__ . '/../config/error_handler.php';
 
 use App\Core\View;
