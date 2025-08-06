937f56d8fcc2e7ed275846de8b3fd35b4571f393
makan-kencing <dizzynight2096@gmail.com>
2025-08-06 19:27:49 +0800

    refactor: move loading admin.css to admin/_base.php

--- web/views/webstore/_base.php
+++ web/views/webstore/_base.php
@@ -5,6 +5,14 @@ declare(strict_types=1);
 ob_start();
 ?>
 
+<link rel="stylesheet" href="/static/styles/webstore.css">
+
+<?php
+$extraHead = ob_get_clean();
+
+ob_start();
+?>
+
 <?php include __DIR__ . "/_header.php" ?>
 
 <?= $content ?? '' ?>
