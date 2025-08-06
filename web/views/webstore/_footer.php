a93a9160f591d49d62a010803352e3f2059eedb1
makan-kencing <dizzynight2096@gmail.com>
2025-08-07 04:02:34 +0800

    feat: mock up for webstore footer

--- web/views/webstore/_footer.php
+++ web/views/webstore/_footer.php
@@ -1,3 +1,45 @@
 <?php
 
 declare(strict_types=1);
+
+?>
+
+<footer>
+    <div>
+        <div>
+            <h3>Shopping</h3>
+            <ul>
+                <li><a href="">...</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>About Us</h3>
+            <ul>
+                <li><a href="">About NNN</a></li>
+                <li><a href="">Affiliates</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Quick Help</h3>
+            <ul>
+                <li><a href="">Help Center</a></li>
+                <li><a href="">FAQ</a></li>
+                <li><a href="">Order Status</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Shop by Category</h3>
+            <ul>
+                <li><a href="">Books</a></li>
+                <li><a href="">Fiction</a></li>
+                <li><a href="">Non-Fiction</a></li>
+                <li><a href="">Young Adults</a></li>
+                <li><a href="">Children's</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
