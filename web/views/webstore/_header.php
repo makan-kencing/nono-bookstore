89d9b4af5fc9f1b0b01664733ab7dbb0e95312a4
makan-kencing <dizzynight2096@gmail.com>
2025-08-07 03:45:39 +0800

    feat: mock up for webstore header

--- web/views/webstore/_header.php
+++ web/views/webstore/_header.php
@@ -1,3 +1,47 @@
 <?php
 
 declare(strict_types=1);
+
+?>
+
+<header>
+    <div>
+        <nav class="contents">
+            <ul id="store-information" class="bar-list">
+                <li><a href="/stores"><i class="fa-solid fa-location-dot"></i>Stores</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+
+            <ul id="store-account" class="bar-list">
+                <li><a href="/account"><i class="fa-solid fa-circle-user"></i>Account</a></li>
+                <li><a href="/wishlist"><i class="fa-regular fa-heart show"></i>Wishlist</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <div>
+        <h1 id="store-logo"><a href="/">Novelty N' Nonsense</a></h1>
+
+        <search>
+            <form action="/search" method="get">
+                <label for="search"></label>
+                <input type="search" name="q" id="search" placeholder="Search by Title, Author, Keyword, or ISBN">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </search>
+
+        <div id="store-cart"><i class="fa-solid fa-cart-shopping"></i></div>
+    </div>
+
+    <div>
+        <nav class="contents">
+            <ul id="store-categories" class="bar-list">
+                <li><a href="">Bestsellers</a></li>
+                <li><a href="">Fiction</a></li>
+                <li><a href="">Non-Fiction</a></li>
+                <li><a href="">Young Adults</a></li>
+                <li><a href="">Children's</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
