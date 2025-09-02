ebcfe5fa42fbc00d3a1426d1badef8f9e819c238
makan-kencing <dizzynight2096@gmail.com>
2025-09-02 15:21:04 +0800

    refactor: add validation to PageRequest

--- web/src/Orm/Expr/PageRequest.php
+++ web/src/Orm/Expr/PageRequest.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace App\Orm\Expr;
 
+use InvalidArgumentException;
+
 readonly class PageRequest
 {
     public function __construct(
-        public int $size,
-        public int $page = 1
+        public int $page = 1,
+        public int $pageSize = 1
     ) {
+        if ($this->page < 1)
+            throw new InvalidArgumentException('Page should be more than or equal to 1, not ' . $this->page);
+        if ($this->pageSize < 1)
+            throw new InvalidArgumentException('Page size should be more than or equal to 1, not ' . $this->pageSize);
     }
 }
