bd12f51c8dc5927fcf93273ff3b6104767345618
makan-kencing <dizzynight2096@gmail.com>
2025-09-03 13:54:09 +0800

    feat: book schema add deleted_at

--- sql/webstore/book.sql
+++ sql/webstore/book.sql
@@ -2,15 +2,16 @@ create or replace table book
 (
     id              bigint unsigned auto_increment
         primary key,
-    slug            varchar(255)    not null,
-    isbn            char(13)        not null,
-    title           varchar(255)    not null,
-    description     text            null,
-    publisher_id    bigint unsigned not null,
-    published_date  varchar(20)     not null,
-    number_of_pages int             not null,
-    language        varchar(255)    null,
-    dimensions      varchar(255)    null,
+    slug            varchar(255)                          not null,
+    isbn            char(13)                              not null,
+    title           varchar(255)                          not null,
+    description     text                                  null,
+    publisher_id    bigint unsigned                       not null,
+    published_date  varchar(20)                           not null,
+    number_of_pages int                                   not null,
+    language        varchar(255)                          null,
+    dimensions      varchar(255)                          null,
+    deleted_at      timestamp default current_timestamp() null,
     constraint isbn
         unique (isbn),
     constraint slug
--- sql/webstore/inventory.sql
+++ sql/webstore/inventory.sql
@@ -3,7 +3,7 @@ create or replace table inventory
     id         bigint unsigned auto_increment
         primary key,
     product_id bigint unsigned                        not null,
-    location   enum ('SHELF', 'WAREHOUSE', 'OFFSITE') not null,
+    location   enum ('WAREHOUSE', 'OFFSITE', 'STORE') not null,
     quantity   int unsigned                           not null,
     constraint inventory_ibfk_1
         foreign key (product_id) references product (id)
