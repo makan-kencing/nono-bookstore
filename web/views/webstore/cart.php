9cbef3fd5544a9ce0eb740102e19f25b20ab6138
makan-kencing <dizzynight2096@gmail.com>
2025-09-08 19:42:06 +0800

    feat: add placeholder cart page

--- web/views/webstore/cart.php
+++ web/views/webstore/cart.php
@@ -2,12 +2,20 @@
 
 declare(strict_types=1);
 
-$title = 'Shopping Cart';
+use App\Core\View;
+use App\Entity\Cart\Cart;
+
+assert(isset($cart) && $cart instanceof Cart);
 
 ob_start();
 ?>
-    <p></p>
+    <?php xdebug_var_dump($cart) ?>
 <?php
+
+$title = 'Shopping Cart';
 $content = ob_get_clean();
 
-include __DIR__ . "/_base.php";
+echo View::render(
+    'webstore/_base.php',
+    ['title' => $title, 'content' => $content]
+);
