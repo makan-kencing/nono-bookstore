47b25d4c69da4dc7bee6e63edac944ec8a88ac45
tanyj0612 <tanyijia9558@gmail.com>
2025-09-09 23:21:08 +0800

    fix :complete the out put

--- web/views/admin/orders.php
+++ web/views/admin/orders.php
@@ -10,67 +10,62 @@ assert(isset($orders) && is_array($orders));
 $title = "Orders";
 ob_start();
 ?>
-    <main class="table">
-        <section class=table_header">
-            <h1>Order Detail List</h1>
-        </section>
-        <section class="table_body">
-            <table>
-                <thead>
-                <tr>
-                    <th>Num</th>
-                    <th>Username</th>
-                    <th>Order Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Shipment Status</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <?php $num = 1; ?>
-                <?php foreach ($orders as $order) : ?>
-                    <tbody>
+    <section class="profile-container">
+        <div class="profile-card">
+
+            <div class="table-wrapper">
+                <table class="user-table" id="user-table">
+                    <thead>
                     <tr>
-                        <td><?= $num ?></td>
-                        <td><?= $order->user->username ?></td>
-
-                        <?php foreach ($order->items as $item) : ?>
-                            <?php $book = $item->book; ?>
-
-                            <?php foreach ($book->images as $image) : ?>
-                                <td><img src="<?= $image->file->filepath ?>" alt="<?= $image->file->alt?>"></td>
-                                <td><?= $book->isbn ?></td>
-                                <td><?= $item->quantity ?></td>
-                                <td>
-                                    <?php if($order->shipment==null) : ?>
-                                        <?php ?>
-                                    <?php else : ?>
-                                        <?php if (!$order->shipment->readyAt) : ?>
-                                            <span class="chip chip-ok">Preparing</span>
-                                        <?php elseif ($order->shipment->readyAt) : ?>
-                                            <span class="chip chip-ok">Ready To Ship</span>
-                                        <?php elseif ($order->shipment->shippedAt) : ?>
-                                            <span class="chip chip-ok">Ship Out</span>
-                                        <?php elseif ($order->shipment->deliveredAt) : ?>
-                                            <span class="chip chip-ok">Delivered</span>
-                                        <?php else : ?>
-                                            <span class="chip chip-no">Error</span>
-                                        <?php endif; ?>
-                                    <?php endif; ?>
-                                </td>
-                                <td>
-
-                                </td>
-                            <?php endforeach; ?>
-                        <?php endforeach; ?>
+                        <th>Num</th>
+                        <th>Username</th>
+                        <th>Ref No</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Shipment Status</th>
                     </tr>
+                    </thead>
+                    <tbody>
+                    <?php $num = 1; ?>
+                    <?php foreach ($orders as $order) : ?>
+                        <tr>
+                            <td><?= $num ?></td>
+                            <td><?= $order->user->username ?></td>
+                            <td><?= $order->refNo ?></td>
+                            <?php foreach ($order->items as $item) : ?>
+                                <td><?= $item->quantity ?></td>
+                            <?php endforeach; ?>
+                            <?php foreach ($order->adjustments as $adjustment) : ?>
+                                <td><?= $adjustment->amount ?></td>
+                            <?php endforeach; ?>
+                            <td class="chip">
+                                <?php if ($order->shipment == null) : ?>
+                                    <span class="chip chip-error">No Shipment</span>
+                                <?php elseif (!$order->shipment->readyAt) : ?>
+                                    <span class="chip chip-preparing">Preparing</span>
+                                <?php elseif ($order->shipment->readyAt && !$order->shipment->shippedAt) : ?>
+                                    <span class="chip chip-ready">Ready To Ship</span>
+                                <?php elseif ($order->shipment->shippedAt && !$order->shipment->deliveredAt) : ?>
+                                    <span class="chip chip-shipped">Shipped</span>
+                                <?php elseif ($order->shipment->deliveredAt) : ?>
+                                    <span class="chip chip-delivered">Delivered</span>
+                                <?php else : ?>
+                                    <span class="chip chip-error">Error</span>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                        <?php $num++; ?>
+                    <?php endforeach; ?>
                     </tbody>
-                    <?php $num++ ?>
-                <?php endforeach; ?>
-
-            </table>
-        </section>
-    </main>
+                </table>
+            </div>
+        </div>
+    </section>
+    <script>
+        $("table.user-table tbody tr").click(/** @param {jQuery.Event} e */ (e) => {
+            window.location = `/admin/order/${e.currentTarget.dataset.id}`;
+        })
+    </script>
 
 <?php
 $content = ob_get_clean();
