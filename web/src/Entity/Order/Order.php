5df543530b59c35352c5aa1b8a57390cb66e6d78
makan-kencing <dizzynight2096@gmail.com>
2025-09-09 11:18:13 +0800

    fix: make Order and OrderItem standalone class instead of extending Cart

--- web/src/Entity/Order/Order.php
+++ web/src/Entity/Order/Order.php
@@ -4,26 +4,34 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
-use App\Entity\Cart\Cart;
+use App\Entity\User\User;
+use App\Orm\Attribute\Id;
 use App\Orm\Attribute\OneToMany;
 use App\Orm\Attribute\OneToOne;
+use App\Orm\Entity;
 use DateTime;
 
-class Order extends Cart
+class Order extends Entity
 {
+    #[Id]
+    public ?int $id;
+
+    #[OneToOne]
+    public User $user;
+
     public string $refNo;
 
     public DateTime $orderedAt;
+
+    /** @var OrderItem[] */
+    #[OneToMany(OrderItem::class, mappedBy: 'order')]
+    public array $items;
 
     #[OneToOne(mappedBy: 'order')]
     public ?Invoice $invoice;
 
     #[OneToOne(mappedBy: 'order')]
     public ?Shipment $shipment;
-
-    /** @var OrderItem[] */
-    #[OneToMany(OrderItem::class, mappedBy: 'order')]
-    public array $items;
 
     /** @var OrderAdjustment[] */
     #[OneToMany(OrderAdjustment::class, mappedBy: 'order')]
--- web/src/Entity/Order/OrderItem.php
+++ web/src/Entity/Order/OrderItem.php
@@ -4,14 +4,25 @@ declare(strict_types=1);
 
 namespace App\Entity\Order;
 
-use App\Entity\Cart\CartItem;
+use App\Entity\Book\Book;
 use App\Entity\Trait\Commentable;
+use App\Orm\Attribute\Id;
+use App\Orm\Attribute\ManyToOne;
+use App\Orm\Entity;
 
-class OrderItem extends CartItem
+class OrderItem extends Entity
 {
     use Commentable;
 
-    public int $unitPrice;
+    #[Id]
+    #[ManyToOne]
+    public Order $order;
+
+    #[Id]
+    #[ManyToOne]
+    public Book $book;
 
     public int $quantity;
+
+    public int $unitPrice;
 }
