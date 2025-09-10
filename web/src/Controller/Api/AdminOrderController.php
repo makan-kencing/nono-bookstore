fb40ee0f576aacb5c877ac729f106155893c2413
tanyj0612 <tanyijia9558@gmail.com>
2025-09-10 19:45:28 +0800

    feat :add selection for order list and order detail

--- web/src/Controller/Api/AdminOrderController.php
+++ web/src/Controller/Api/AdminOrderController.php
@@ -4,14 +4,44 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Controller\Api\ApiController;
+use App\Core\View;
+use App\DTO\Request\ShipmentUpdateDTO;
 use App\Entity\User\UserRole;
+use App\Exception\BadRequestException;
+use App\Exception\ForbiddenException;
+use App\Exception\NotFoundException;
+use App\Exception\UnprocessableEntityException;
 use App\Router\AuthRule;
+use App\Router\Method\PUT;
 use App\Router\Path;
 use App\Router\RequireAuth;
+use App\Service\OrderService;
+use App\Service\UserService;
 
 #[Path('/api/orderList')]
 #[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL, redirect: false)]
 readonly class AdminOrderController extends ApiController
 {
 
+    private OrderService $orderService;
+    public function __construct(PDO $pdo, View $view)
+    {
+        parent::__construct($pdo, $view);
+        $this->orderService = new OrderService($this->pdo);
+    }
+
+    /**
+     * @throws ForbiddenException
+     * @throws BadRequestException
+     * @throws UnprocessableEntityException
+     * @throws NotFoundException
+     */
+    #[Put]
+    #[Path('/updateShipmentStatus')]
+    public function updateShipmentStatus(): void
+    {
+        $dto=ShipmentUpdateDTO::jsonDeserialize(self::getJsonBody());
+        $this->orderService->update($dto);
+        http_response_code(204);
+    }
 }
---
+++ web/src/Controller/Web/AdminOrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Web;
+
+use App\Controller\Web\WebController;
+use App\Core\View;
+use App\Exception\NotFoundException;
+use App\Repository\OrderRepository;
+use App\Repository\Query\OrderCriteria;
+use App\Repository\Query\OrderQuery;
+use App\Router\Method\GET;
+use App\Router\Path;
+use PDO;
+
+
+#[Path('/admin/order')]
+//#[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL)]
+readonly class AdminOrderController extends WebController
+{
+    private OrderRepository $orderRepository;
+
+    public function __construct(PDO $pdo, View $view)
+    {
+        parent::__construct($pdo, $view);
+        $this->orderRepository = new OrderRepository($this->pdo);
+    }
+    #[GET]
+    #[Path('/{id}')]
+    public function viewOrder(string $id): void
+    {
+        $qb =OrderQuery::orderListings()
+            ->where(OrderCriteria::byId(alias:'o'))
+            ->bind(':id', $id);
+        $order = $this->orderRepository->getOne($qb);
+
+        if ($order===null) {
+            throw new NotFoundException();
+        }
+        echo $this->render('admin/order/order.php', ['order' => $order]);
+    }
+
+
+}
