1974c3ac52d187b9f8c6e945dd2642286e66e7db
tanyj0612 <tanyijia9558@gmail.com>
2025-09-10 22:57:28 +0800

    feat : user controller

---
+++ web/src/Controller/Api/UserOrderController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Controller\Api\ApiController;
+
+readonly class UserOrderController extends ApiController
+{
+
+}
--- web/src/Controller/Web/UserAccountController.php
+++ web/src/Controller/Web/UserAccountController.php
@@ -6,13 +6,17 @@ namespace App\Controller\Web;
 
 use App\Router\Method\GET;
 use App\Router\Path;
+use App\Router\RequireAuth;
+
 #[Path('/user')]
-readonly class UserProfileController extends WebController
+#[RequireAuth]
+readonly class UserAccountController extends WebController
 {
     #[GET]
     #[Path('/profile')]
     public function viewProfile(): void
     {
+
         echo $this->render('user/userProfile.php');
     }
 }
