6b03d8fab07ac3245d0ad127e57387399f2f83d4
tanyj0612 <tanyijia9558@gmail.com>
2025-09-10 19:34:04 +0800

    feat : order.php for webstore

--- web/views/webstore/orders.php
+++ web/views/webstore/orders.php
@@ -2,11 +2,42 @@
 
 declare(strict_types=1);
 
+use App\Entity\User\User;
+
+assert(isset($user) && $user instanceof User );
 $title = 'Order History';
 
 ob_start();
 ?>
-    <p></p>
+
+    <div class="table-wrapper">
+        <h1>Order Detail List</h1>
+        <table class="user-table">
+            <thead>
+            <tr>
+                <th>Num</th>
+                <th>Ref No</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Shipment Status</th>
+            </tr>
+            </thead>
+            <tbody><?php $num=1;?>
+
+            <?php foreach ($user->orders as $order): ?>
+                <tr data-id="<?= $order->id ?>">
+                    <td><?= $num?></td>
+                    <td><?=$order->refNo?></td>
+                    <?php foreach ($order->items as $item): ?>
+                        <td><?= $item->quantity?></td>
+                    <?php endforeach; ?>
+                </tr>
+                <?php $num++;?>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
 <?php
 $content = ob_get_clean();
 
