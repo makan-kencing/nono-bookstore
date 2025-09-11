0d8d0fe636c1b8aa1c7d68630de91e4ebe68d1a5
makan-kencing <dizzynight2096@gmail.com>
2025-09-12 01:01:45 +0800

    feat: placeholder crud controller for book

---
+++ web/src/Controller/Api/BookController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Entity\User\UserRole;
+use App\Router\AuthRule;
+use App\Router\Method\DELETE;
+use App\Router\Method\POST;
+use App\Router\Method\PUT;
+use App\Router\Path;
+use App\Router\RequireAuth;
+
+#[Path('/api/book')]
+readonly class BookController extends ApiController
+{
+    #[POST]
+    #[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL, redirect: false)]
+    public function createBook(): void
+    {
+        $dto = null;
+
+        http_response_code(201);
+    }
+
+    #[PUT]
+    #[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL, redirect: false)]
+    public function updateBook(string $id): void
+    {
+
+    }
+
+    #[DELETE]
+    #[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL, redirect: false)]
+    public function deleteBook(string $id): void
+    {
+
+    }
+}
