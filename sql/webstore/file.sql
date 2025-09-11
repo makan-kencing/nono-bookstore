8cf6d51f6d2d2d02e59f40ddf2b1e5629634cb16
makan-kencing <dizzynight2096@gmail.com>
2025-09-11 12:00:02 +0800

    feat: add local_filepath to File

--- sql/webstore/file.sql
+++ sql/webstore/file.sql
@@ -1,13 +1,14 @@
 create or replace table file
 (
-    id         bigint unsigned auto_increment
+    id             bigint unsigned auto_increment
         primary key,
-    user_id    bigint unsigned                       not null,
-    filename   varchar(255)                          not null,
-    mimetype   varchar(255)                          not null,
-    alt        varchar(255)                          null,
-    filepath   varchar(255)                          not null,
-    created_at timestamp default current_timestamp() not null,
+    user_id        bigint unsigned                       not null,
+    filename       varchar(255)                          not null,
+    mimetype       varchar(255)                          not null,
+    alt            varchar(255)                          null,
+    filepath       varchar(255)                          not null,
+    local_filepath varchar(255)                          null,
+    created_at     timestamp default current_timestamp() not null,
     constraint file_ibfk_1
         foreign key (user_id) references user (id)
 );
--- web/src/Entity/File.php
+++ web/src/Entity/File.php
@@ -26,5 +26,7 @@ class File extends Entity
 
     public string $filepath;
 
+    public string $localFilepath;
+
     public DateTime $createdAt;
 }
