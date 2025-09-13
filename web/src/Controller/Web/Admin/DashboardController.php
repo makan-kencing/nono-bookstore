7466ad1b7238161c0b6d70e3651ccae040008737
tanyj0612 <tanyijia9558@gmail.com>
2025-09-14 00:26:56 +0800

    feat: add chat controller

--- web/src/Controller/Web/Admin/DashboardController.php
+++ web/src/Controller/Web/Admin/DashboardController.php
@@ -5,19 +5,35 @@ declare(strict_types=1);
 namespace App\Controller\Web\Admin;
 
 use App\Controller\Web\WebController;
+use App\Core\View;
 use App\Entity\User\UserRole;
 use App\Router\AuthRule;
 use App\Router\Method\GET;
 use App\Router\Path;
 use App\Router\RequireAuth;
+use App\Service\OrderService;
+use DateTime;
+use PDO;
 
 #[Path('/admin')]
 #[RequireAuth([UserRole::STAFF], rule: AuthRule::HIGHER_OR_EQUAL)]
 readonly class DashboardController extends WebController
 {
+    private OrderService $orderService;
+
+    public function __construct(PDO $pdo, View $view)
+    {
+        parent::__construct($pdo, $view);
+        $this->orderService = new OrderService($pdo);
+    }
+
     #[GET]
     public function viewDashboard(): void
     {
-        echo $this->render('admin/dashboard.php');
+        $from = new DateTime('2000-01-01');
+        $to = new DateTime('2100-01-01');
+        list($monthlySales, $categorySales) = $this->orderService->getSalesMetrics($from, $to);
+
+        echo $this->render('admin/dashboard.php', ['monthlySales' => $monthlySales, 'categorySales' => $categorySales]);
     }
 }
