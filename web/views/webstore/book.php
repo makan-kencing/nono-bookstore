4423bc8518be232e98fdd769fcaf15d2edd9bb15
makan-kencing <dizzynight2096@gmail.com>
2025-09-15 11:48:14 +0800

    feat: add alert on add cart

--- web/views/webstore/book.php
+++ web/views/webstore/book.php
@@ -24,173 +24,177 @@ $template = new Template(
 <?php $template->endFragment(); ?>
 
 <?php $template->start() ?>
-    <main>
-        <nav class="breadcrumb">
-            <a href="/">Home</a>
+<main>
+    <nav class="breadcrumb">
+        <a href="/">Home</a>
+        >
+        <a href="/books/search">Books</a>
+        <?php if ($category = $work->getPrimaryCategory() ?? null): ?>
             >
-            <a href="/books/search">Books</a>
-            <?php if ($category = $work->getPrimaryCategory() ?? null): ?>
-                >
-                <?= $category->category->name ?>
-            <?php endif; ?>
-        </nav>
+            <?= $category->category->name ?>
+        <?php endif; ?>
+    </nav>
 
-        <section id="book-listing">
-            <div id="book-preview" class="carousel">
-                <div class="carousel-images">
-                    <?php foreach ($book->images as $image) : ?>
-                        <div style="order: <?= $image->imageOrder ?>; display: flex;">
-                            <img src="<?= $image->file->filepath ?>" alt="<?= $image->file->alt ?>" style="height: max-content">
-                        </div>
+    <section id="book-listing">
+        <div id="book-preview" class="carousel">
+            <div class="carousel-images">
+                <?php foreach ($book->images as $image) : ?>
+                    <div style="order: <?= $image->imageOrder ?>; display: flex;">
+                        <img src="<?= $image->file->filepath ?>" alt="<?= $image->file->alt ?>"
+                             style="height: max-content">
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+
+        <div id="book-information">
+            <h2><?= $work->title ?></h2>
+
+            <div>by
+                <?=
+                implode(', ', array_map(
+                    fn(AuthorDefinition $author) => "<span>{$author->author->name}</span>",
+                    $book->authors
+                ))
+                ?>
+            </div>
+
+            <?php if ($work->series) : ?>
+                <p>
+                    <span>Part of: <?= $work->series->series->name ?></span>
+                </p>
+            <?php endif; ?>
+        </div>
+
+        <div id="book-purchase">
+            <form action="/api/cart" id="add-to-cart">
+                <input type="hidden" name="book_id" value="<?= $book->id ?>">
+
+                <div id="product-variants">
+                    <?php foreach ($work->books as $otherBook): ?>
+                        <?php if ($otherBook->getCurrentPrice() === null) continue; ?>
+                        <?php if ($otherBook->id === $book->id): ?>
+                            <div class="product-variant">
+                                <p class="type"><?= $otherBook->coverType->title() ?></p>
+                                <p><?= $otherBook->language ?></p>
+                                <p class="price">
+                                    RM <?= number_format($otherBook->getCurrentPrice()->amount / 100, 2) ?></p>
+                            </div>
+                        <?php else: ?>
+                            <a href="/book/<?= $otherBook->isbn ?>/<?= $work->slug ?>"
+                               class="product-variant">
+                                <p class="type"><?= $otherBook->coverType->title() ?></p>
+                                <p><?= $otherBook->language ?></p>
+                                <p class="price">
+                                    RM <?= number_format($otherBook->getCurrentPrice()->amount / 100, 2) ?></p>
+                            </a>
+                        <?php endif; ?>
                     <?php endforeach; ?>
                 </div>
-            </div>
 
-            <div id="book-information">
-                <h2><?= $work->title ?></h2>
-
-                <div>by
-                    <?=
-                    implode(', ', array_map(
-                        fn(AuthorDefinition $author) => "<span>{$author->author->name}</span>",
-                        $book->authors
-                    ))
-                    ?>
-                </div>
-
-                <?php if ($work->series) : ?>
-                    <p>
-                        <span>Part of: <?= $work->series->series->name ?></span>
-                    </p>
-                <?php endif; ?>
-            </div>
-
-            <div id="book-purchase">
-                <form action="/api/cart" id="add-to-cart">
-                    <input type="hidden" name="book_id" value="<?= $book->id ?>">
-
-                    <div id="product-variants">
-                        <?php foreach ($work->books as $otherBook): ?>
-                            <?php if ($otherBook->getCurrentPrice() === null) continue; ?>
-                            <?php if ($otherBook->id === $book->id): ?>
-                                <div class="product-variant">
-                                    <p class="type"><?= $otherBook->coverType->title() ?></p>
-                                    <p><?= $otherBook->language ?></p>
-                                    <p class="price">RM <?= number_format($otherBook->getCurrentPrice()->amount / 100, 2) ?></p>
-                                </div>
-                            <?php else: ?>
-                                <a href="/book/<?= $otherBook->isbn ?>/<?= $work->slug ?>"
-                                   class="product-variant">
-                                    <p class="type"><?= $otherBook->coverType->title() ?></p>
-                                    <p><?= $otherBook->language ?></p>
-                                    <p class="price">RM <?= number_format($otherBook->getCurrentPrice()->amount / 100, 2) ?></p>
-                                </a>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
+                <div id="product-ordering">
+                    <?php $inventory = $book->getClosestStock() ?>
+                    <?php $total = $book->getTotalInStock() ?>
+                    <div id="product-stocks">
+                        <?php if ($total > 0): ?>
+                            <p><i class="fa-solid fa-check"></i>
+                                In stock.
+                                <?php if ($total < 50): ?>
+                                    (<?= $total ?> left)
+                                <?php endif; ?>
+                            </p>
+                            <p><?= $inventory->location->getEstimatedShipping() ?></p>
+                        <?php else: ?>
+                            <p><i class="fa-solid fa-x"></i> No products in stock</p>
+                        <?php endif; ?>
                     </div>
 
-                    <div id="product-ordering">
-                        <?php $inventory = $book->getClosestStock() ?>
-                        <?php $total = $book->getTotalInStock() ?>
-                        <div id="product-stocks">
-                            <?php if ($total > 0): ?>
-                                <p><i class="fa-solid fa-check"></i>
-                                    In stock.
-                                    <?php if ($total < 50): ?>
-                                        (<?= $total ?> left)
-                                    <?php endif; ?>
-                                </p>
-                                <p><?= $inventory->location->getEstimatedShipping() ?></p>
-                            <?php else: ?>
-                                <p><i class="fa-solid fa-x"></i> No products in stock</p>
-                            <?php endif; ?>
-                        </div>
-
-                        <?php if ($total > 0): ?>
+                    <?php if ($total > 0): ?>
                         <div id="product-quantity">
                             <label for="quantity">Quantity</label>
                             <input type="number" name="quantity" id="quantity"
                                    value="1" min="1" max="<?= $total ?>" step="1">
                             <button type="submit">Add to Cart</button>
                         </div>
-                        <?php endif; ?>
-                    </div>
-                </form>
+                    <?php endif; ?>
+                </div>
+            </form>
+        </div>
+    </section>
 
-                <script>
-                    $("#add-to-cart").submit(/** @param {jQuery.Event} e */(e) => {
-                        e.preventDefault();
+    <section id="synopsis">
+        <h3>Synopsis</h3>
 
-                        const data = new FormData(e.target);
+        <div>
+            <p><?= $book->description ?></p>
+        </div>
+    </section>
 
-                        $.ajax(
-                            e.target.action,
-                            {
-                                method: "PATCH",
-                                data: JSON.stringify(Object.fromEntries(data.entries())),
-                                error: (jqXHR, textStatus, errorThrown) => {
-                                    console.error(jqXHR, textStatus, errorThrown)
-                                },
-                                success: () => {
-                                }
-                            }
-                        );
-                    });
-                </script>
-            </div>
-        </section>
+    <section id="book-details">
+        <h3>Product Details</h3>
 
-        <section id="synopsis">
-            <h3>Synopsis</h3>
-
-            <div>
-                <p><?= $book->description ?></p>
-            </div>
-        </section>
-
-        <section id="book-details">
-            <h3>Product Details</h3>
-
-            <table>
+        <table>
+            <tr>
+                <td>ISBN-13</td>
+                <td><?= $book->isbn ?></td>
+            </tr>
+            <tr>
+                <td>Publisher</td>
+                <td><?= $book->publisher ?></td>
+            </tr>
+            <tr>
+                <td>Publication Date</td>
+                <td><?= $book->publicationDate ?></td>
+            </tr>
+            <?php if ($work->series !== null) : ?>
                 <tr>
-                    <td>ISBN-13</td>
-                    <td><?= $book->isbn ?></td>
+                    <td>Series</td>
+                    <td><?= $work->series->series->name ?></td>
                 </tr>
+            <?php endif; ?>
+            <tr>
+                <td>Pages</td>
+                <td><?= $book->numberOfPages ?></td>
+            </tr>
+            <?php if ($book->dimensions !== null): ?>
                 <tr>
-                    <td>Publisher</td>
-                    <td><?= $book->publisher ?></td>
+                    <td>Product dimensions</td>
+                    <td><?= $book->dimensions ?></td>
                 </tr>
-                <tr>
-                    <td>Publication Date</td>
-                    <td><?= $book->publicationDate ?></td>
-                </tr>
-                <?php if ($work->series !== null) : ?>
-                    <tr>
-                        <td>Series</td>
-                        <td><?= $work->series->series->name ?></td>
-                    </tr>
-                <?php endif; ?>
-                <tr>
-                    <td>Pages</td>
-                    <td><?= $book->numberOfPages ?></td>
-                </tr>
-                <?php if ($book->dimensions !== null): ?>
-                    <tr>
-                        <td>Product dimensions</td>
-                        <td><?= $book->dimensions ?></td>
-                    </tr>
-                <?php endif; ?>
-                <?php if ($book->editionInformation !== null): ?>
+            <?php endif; ?>
+            <?php if ($book->editionInformation !== null): ?>
                 <tr>
                     <td>Edition Information</td>
                     <td><?= $book->editionInformation ?></td>
                 </tr>
-                <?php endif; ?>
-                <tr>
-                    <td>Language</td>
-                    <td><?= $book->language ?></td>
-                </tr>
-            </table>
-        </section>
-    </main>
+            <?php endif; ?>
+            <tr>
+                <td>Language</td>
+                <td><?= $book->language ?></td>
+            </tr>
+        </table>
+    </section>
+</main>
+
+<script>
+    $("#add-to-cart").submit(/** @param {jQuery.Event} e */(e) => {
+        e.preventDefault();
+
+        const data = new FormData(e.target);
+
+        $.ajax(
+            e.target.action,
+            {
+                method: "PATCH",
+                data: JSON.stringify(Object.fromEntries(data.entries())),
+                error: (jqXHR, textStatus, errorThrown) => {
+                    console.error(jqXHR, textStatus, errorThrown)
+                },
+                success: () => {
+                    alert("Added to cart");
+                }
+            }
+        );
+    });
+</script>
 <?= $template->end() ?>
